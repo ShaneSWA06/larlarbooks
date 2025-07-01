c00a98792e7ec87f947d7b2233a7a5a3e0db4502
shaneswa06 <shane.24@ichat.sp.edu.sg>
2025-07-02 02:09:41 +0800

    correct directory fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,8 +20,8 @@ const App = () => (
           <Sonner />
           <BrowserRouter>
             <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/about" element={<About />} />
+              <Route path="/larlarbooks" element={<Index />} />
+              <Route path="/larlarbooks/about" element={<About />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
