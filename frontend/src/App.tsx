74b3baf56c8b12e55fc700cfe0d0a95377db9d37
shaneswa06 <shane.24@ichat.sp.edu.sg>
2025-07-05 21:32:28 +0800

    Add homepage and basename for GitHub Pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/larlarbooks">
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/about" element={<About />} />
